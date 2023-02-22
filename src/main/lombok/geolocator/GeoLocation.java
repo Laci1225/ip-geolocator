ff49bae056f93fdf019072ba8c0ca96340d07e5e
Laci <nagylacika2002@gmail.com>
2023-02-22 15:25:24 +0100

    Deleting the now unused commons-lang3 dependency from pom.xml

--- src/main/lombok/geolocator/GeoLocation.java
+++ src/main/lombok/geolocator/GeoLocation.java
@@ -1,11 +1,5 @@
 package geolocator;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
-/*public static String toString() {
-    return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
-}*/
 public class GeoLocation {
 
     private String ip;
